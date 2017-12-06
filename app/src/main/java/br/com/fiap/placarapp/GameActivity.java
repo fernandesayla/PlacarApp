596f1c5fcfdc5a4f606b2e6eea5b55c9d6a2e994
logonrm <logonrm@fiap.com.br>
2017-12-06 21:50:20 -0200

    Aula 2

--- app/src/main/java/br/com/fiap/placarapp/GameActivity.java
+++ app/src/main/java/br/com/fiap/placarapp/GameActivity.java
@@ -1,9 +1,18 @@
 package br.com.fiap.placarapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -13,6 +22,17 @@ public class GameActivity extends AppCompatActivity {
     private TextView tvTimeCasa;
     private TextView tvTimeVisitante;
 
+    private TextView tvPlacarCasa;
+    private TextView tvPlacarVisitante;
+    private int golCasa;
+    private int golVisitante;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +40,8 @@ public class GameActivity extends AppCompatActivity {
 
         tvTimeCasa = (TextView) findViewById(R.id.tvTimeCasa);
         tvTimeVisitante = (TextView) findViewById(R.id.tvTimeVisitante);
-
+        tvPlacarCasa = (TextView) findViewById(R.id.tvPlacarCasa);
+        tvPlacarVisitante = (TextView) findViewById(R.id.tvPlacarVisitante);
 
         if (getIntent() != null) {
             timeCasa = getIntent().getStringExtra("TIMECASA");
@@ -31,5 +52,77 @@ public class GameActivity extends AppCompatActivity {
             // R.id.tvAgTimeCasa = timeCasa;
 
         }
+        if (savedInstanceState != null){
+            golCasa = savedInstanceState.getInt("CASA");
+            golVisitante = savedInstanceState.getInt("VISITANTE");
+        }
+        tvPlacarCasa.setText(String.valueOf(golCasa));
+        tvPlacarVisitante.setText(String.valueOf(golVisitante));
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("CASA", golCasa);
+        outState.putInt("VISITANTE", golVisitante);
+
+    }
+
+    public void golCasa(View v) {
+
+        golCasa++;
+        tvPlacarCasa.setText(String.valueOf(golCasa));
+
+
+
+    }
+
+
+    public void golVisitante(View v) {
+
+        golVisitante++;
+        tvPlacarVisitante.setText(String.valueOf(golVisitante));
+
+
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Game Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
